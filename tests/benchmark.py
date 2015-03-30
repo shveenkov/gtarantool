7ae207bd5faa540ee1069e7c6d3e4c28e2be2286
shveenkov <shveenkov@mail.ru>
2015-03-30 18:47:27 +0300

    fix by benchmark

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -35,6 +35,16 @@ for it in range(100000):
 t2 = time.time()
 benchmark["tarantool"]["select"] = t2 - t1
 
+# update test
+print("tarantool update test")
+t1 = time.time()
+for it in range(100000):
+    r = tnt.update("tester", it, [("=", 2, it)])
+
+
+t2 = time.time()
+benchmark["tarantool"]["update"] = t2 - t1
+
 # delete test
 print("tarantool delete test")
 t1 = time.time()
@@ -63,6 +73,14 @@ def select_job(tnt):
     for i in range(2500):
         cnt += 1
         r = tnt.select("tester", cnt)
+
+
+def update_job(tnt):
+    global cnt
+
+    for i in range(2500):
+        cnt += 1
+        r = tnt.update("tester", cnt, [("=", 2, cnt)])
 
 
 def delete_job(tnt):
@@ -97,6 +115,17 @@ gevent.joinall(jobs)
 t2 = time.time()
 benchmark["gtarantool"]["select"] = t2 - t1
 
+# update test
+print "gtarantool update test"
+t1 = time.time()
+cnt = 0
+jobs = [gevent.spawn(update_job, tnt)
+        for _ in range(40)]
+
+gevent.joinall(jobs)
+t2 = time.time()
+benchmark["gtarantool"]["update"] = t2 - t1
+
 # delete test
 print "gtarantool delete test"
 t1 = time.time()
@@ -110,5 +139,5 @@ benchmark["gtarantool"]["delete"] = t2 - t1
 
 print("\nbenchmark results:")
 print("call    tarantool  gtarantool")
-for k in ("insert", "select", "delete"):
+for k in ("insert", "select", "update", "delete"):
     print("{2:6}: {0:0.6f}  {1:0.6f}".format(benchmark["tarantool"][k], benchmark["gtarantool"][k], k))
